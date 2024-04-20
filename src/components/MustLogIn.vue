d2c094e51b3d2ec7a2bb972b90f2a2807b5e832d
foxikle <tristantroop260@gmail.com>
2024-04-19 21:44:49 -0400

    Create a simple component to redirect to the login page

---
+++ src/components/MustLogIn.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1 class="text-white text-center text-lg m-12">You must be logged in to view this page. <br>
+      <router-link class="underline text-blue-500" to="/auth/login">Log In</router-link>
+    </h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "LogIn"
+}
+</script>
