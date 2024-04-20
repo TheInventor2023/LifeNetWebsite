ff025b4e0b25610784d7503cfdd8f8d4895de470
foxikle <tristantroop260@gmail.com>
2024-04-19 21:37:58 -0400

    Cookie annoying

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <Header/>
+  <CookieAnnoyance/>
   <router-view/>
   <Footer/>
 </template>
 <script setup>
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue"
+import CookieAnnoyance from "@/components/CookieAnnoyance.vue";
 </script>
 <style>
 body {
