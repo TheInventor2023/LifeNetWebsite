ff8f4e15f1bfe34c4dd5175f7a2efb3cae3afb83
foxikle <tristantroop260@gmail.com>
2024-04-19 21:39:01 -0400

    Add vue-toastification, and vuex

--- src/main.js
+++ src/main.js
@@ -4,16 +4,28 @@ import App from './App.vue'
 import router from "@/router/index.js";
 import {library} from '@fortawesome/fontawesome-svg-core';
 import {faDiscord, faGithub} from '@fortawesome/free-brands-svg-icons';
+import {faXmark} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
-
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 import '@/assets/tailwind.css'
+import store from "@/store/user.js";
 
 library.add (
     faDiscord,
-    faGithub
+    faGithub,
+    faXmark,
 )
 
 const app = createApp(App);
+
+app.use(Toast, {
+    transition: 'Vue-Toastification__bounce',
+    maxToasts: 20,
+    newestOnTop: true,
+});
+
 app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(router)
+app.use(store)
 app.mount('#app')
