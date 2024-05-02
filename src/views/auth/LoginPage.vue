83404c548bb175d00b68a4f3d5b3d2ad6bd54517
foxikle <tristantroop260@gmail.com>
2024-05-01 20:38:49 -0400

    Add a catch all error

--- src/views/auth/LoginPage.vue
+++ src/views/auth/LoginPage.vue
@@ -229,6 +229,21 @@ export default {
                 icon: false,
                 rtl: false,
               });
+            } else {
+              useToast().error('An error occoured! Please check the browser console, and report this issue!', {
+                position: 'top-right',
+                timeout: 5000,
+                closeOnClick: true,
+                pauseOnFocusLoss: true,
+                pauseOnHover: true,
+                draggable: true,
+                draggablePercent: 0.6,
+                showCloseButtonOnHover: false,
+                hideProgressBar: false,
+                closeButton: 'button',
+                icon: false,
+                rtl: false,
+              });
             }
           });
     }
