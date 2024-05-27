f8c44a8f8d5032bbd51d7462d9e491c9e04837cf
foxikle <tristantroop260@gmail.com>
2024-05-26 20:28:27 -0400

    Promote prince legacy

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -73,15 +73,15 @@ export default {
           avatar: "https://cdn.discordapp.com/avatars/916642284810936380/8a0974eda5abd6a64fdb9fc8e360a6fc?size=1024"
         },
         {
-          id: 5,
+          id: 6,
           name: "LZ",
           position: "Helper",
           avatar: "https://cdn.discordapp.com/avatars/828916510646468630/09b902b34376ee668442e1e7f15babe1?size=1024"
         },
         {
-          id: 6,
+          id: 5,
           name: "PrinceLegacy",
-          position: "Helper",
+          position: "Mod",
           avatar: "https://cdn.discordapp.com/avatars/1039887622216089682/0f3cc4378c11d877a43ffd9e3e1fde74?size=1024"
         },
       ],
